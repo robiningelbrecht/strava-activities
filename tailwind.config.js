a7f790f385047a4033ffb431e3cd834d3c1ddc5e
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-11-14 20:21:45 +0100

    Table border

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     darkMode: 'class',
     content: [
         "./build/html/*.html",
-        "./build/html/**/*.html",
+        "./build/html/**/activity-8837881433.html",
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
